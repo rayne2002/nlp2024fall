c16cec2b03ec66c622fbd3891df7b54702a89e3c
xz4535 <144400181+xz4535@users.noreply.github.com>
2024-10-08 22:44:20 -0400

    1

--- hw2/utils.py
+++ hw2/utils.py
@@ -49,27 +49,27 @@ def rate(step, model_size, factor, warmup):
 
 
 class PositionalEncoding(nn.Module):
-    "Implement the PE function."
+    "Implement the PE function."
 
-    def __init__(self, d_model, dropout, max_len=5000):
-        super(PositionalEncoding, self).__init__()
-        self.dropout = nn.Dropout(p=dropout)
+    def __init__(self, d_model, dropout, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
 
-        # Compute the positional encodings once in log space.
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len).unsqueeze(1)
-        div_term = torch.exp(
-            torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model)
-        )
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
+        # Compute the positional encodings once in log space.
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model)
+        )
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
 
-        self.register_buffer("pe", pe)
+        self.register_buffer("pe", pe)
 
-    def forward(self, x):
-        pe = self.pe.unsqueeze(0)
-        x = x + pe[:, : x.size(1)].requires_grad_(False)
-        return self.dropout(x)
+    def forward(self, x):
+        pe = self.pe.unsqueeze(0)
+        x = x + pe[:, : x.size(1)].requires_grad_(False)
+        return self.dropout(x)
 
 
 def greedy_decode(model, src, src_mask, max_len, start_symbol):
